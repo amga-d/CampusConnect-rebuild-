e71e660eebd2624ee4cc77917bf05e743d4f5cee
amgad-linux <amgad.abdulrazzaq@gmail.com>
2025-03-28 02:15:31 +0700

    update .gitignore, config eslint and uninstall unused packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-];
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+]);
